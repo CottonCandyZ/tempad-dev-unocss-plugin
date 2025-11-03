57dcbc260f36625a3e89753e06a50f86a4b09f2e
CottonCandyZ <06-arid.zilch@icloud.com>
2025-11-03 19:08:56 +0800

    feat: add line-bold color

--- src/color-tokens.ts
+++ src/color-tokens.ts
@@ -17,6 +17,7 @@ export const COLOR_TOKEN_RULES: ColorTokenRule[] = [
   createRule(/^norm-brand_pink$/i, () => 'brand_pink'),
   createRule(/^norm-brand_blue$/i, () => 'brand_blue'),
   createRule(/^line-line_light$/i, () => 'line_light'),
+  createRule(/^line-line_bold$/i, () => 'line_bold'),
 ]
 
 export function resolveColorTokenName(variableName: string): string | null {
